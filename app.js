b825e684403db711764011d894a147fe531d8d62
Mr raju <143610202+majid598@users.noreply.github.com>
2024-09-18 22:30:00 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
+      "http://localhost:5173",
       "https://portfolio-nine-snowy-67.vercel.app",
       "https://codewithraju.vercel.app",
     ],
